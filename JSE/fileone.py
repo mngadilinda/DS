f0f56d2dce02f593f1742fd9087bd4c160dfbae5
Mngadi Linda <100442560+mngadilinda@users.noreply.github.com>
2023-05-11 12:31:46 +0200

    Add files via upload

---
+++ JSE/fileone.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+
+
+MTN = pd.read_csv('/home/mngadil/Downloads/MTN.JO.csv', index_col='Date', parse_dates=True)
+NDB = pd.read_csv('/home/mngadil/Downloads/NED.JO.csv', index_col='Date', parse_dates=True)
+ZAR = pd.read_csv('/home/mngadil/Downloads/ZAR=X.csv', index_col='Date', parse_dates=True)
+FSR = pd.read_csv('/home/mngadil/Downloads/FSR.JO.csv', index_col='Date', parse_dates=True)
+ABG = pd.read_csv('/home/mngadil/Downloads/ABG.JO.csv', index_col='Date', parse_dates=True)
+
+# print(MTN.head())
+# plt.plot(MTN['Close'])
+# plt.show()
+
+print(FSR.head())
+plt.figure('Banks and MTN share price')
+plt.title('South AFrican Banks + MTN')
+plt.plot(ABG['Close'], color='red', label='ABSA')
+plt.plot(NDB['Close'], color='green', label='NEDBANK')
+plt.plot(MTN['Close'], color='yellow', label='MTN')
+plt.plot(FSR['Close'], color='blue', label='FirstRand')
+plt.legend()
+plt.figure('South African Rand')
+plt.title('South AFrican RAND')
+plt.plot(ZAR['Close'], label='ZAR', color='indigo')
+
+plt.show()
